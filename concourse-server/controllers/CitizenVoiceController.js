7bbb43efe420f3966ba89e34ebf783533bac9702
Alex Isabelle Shuping <alex.i.shuping@gmail.com>
2019-12-19 01:04:32 -0500

    Add testing

--- concourse-server/controllers/CitizenVoiceController.js
+++ concourse-server/controllers/CitizenVoiceController.js
@@ -15,77 +15,149 @@
  */
 
 import { CitizenVoiceModel } from '../models/CitizenVoiceSchema.js'
+import mongodb from 'mongodb'
 
 export const CreateCitizenVoice = (req, res, next) => {
-    const new_voice = new CitizenVoiceModel({
-        name: req.body.name,
-        occupation: req.body.occupation,
-        quote: req.body.quote
-    })
+	if(!(
+		req.body.name
+		&& req.body.occupation
+		&& req.body.quote
+	)){
+		return res.status(400).json({
+			reason: "Missing required fields"
+		})
+	}
+	const new_voice = new CitizenVoiceModel({
+		name: req.body.name,
+		occupation: req.body.occupation,
+		quote: req.body.quote
+	})
 
-    new_voice.save((err) => {
-        if(err){
-            return next(err)
-        }
+	new_voice.save((err) => {
+		if(err){
+			return next(err)
+		}
 
-        res.status(200).json(new_voice)
-    })
+		res.status(200).json(new_voice)
+	})
 }
 
 export const RetrieveCitizenVoice = async (req, res, next) => {
-    const got = await CitizenVoiceModel.findById(req.params.id)
+	const obj_id = req.params.id
 
-    if(got){
-        res.status(200).json(got)
-    }else{
-        res.status(404)
-    }
+	if(!mongodb.ObjectID.isValid(obj_id)){
+		res.status(400).json({
+			reason: "Malformed Object ID"
+		})
+	}else{
+
+		const got = await CitizenVoiceModel.findById(obj_id)
+
+		if(got){
+			res.status(200).json(got)
+		}else{
+			res.sendStatus(404)
+		}
+	}
 }
 
 export const RetrieveAllVoices = async (req, res, next) => {
-    const got = await CitizenVoiceModel.find()
+	const got = await CitizenVoiceModel.find()
 
-    if(got){
-        res.status(200).json(got)
-    }else{
-        res.status(404)
-    }
+	if(got){
+		res.status(200).json(got)
+	}else{
+		res.status(404)
+	}
 }
 
 export const EditCitizenVoice = async (req, res, next) => {
-    const to_edit = await CitizenVoiceModel.findById(req.params.id)
+	const obj_id = req.params.id
 
-    if(!to_edit){
-        return res.sendStatus(404)
-    }
+	if(!mongodb.ObjectID.isValid(obj_id)){
+		return res.status(400).json({
+			reason: "Malformed Object ID"
+		})
+	}else{
+		const to_edit = await CitizenVoiceModel.findById(obj_id)
 
-    if(req.body.name){
-        to_edit.name = req.body.name
-    }
+		if(!to_edit){
+			return res.sendStatus(404)
+		}
 
-    if(req.body.occupation){
-        to_edit.occupation = req.body.occupation
-    }
+		const valid_keys = [
+			"name", "occupation", "quote"
+		]
+		let unknown_keys = []
+		Object.keys(req.body).forEach(function(key){
+			if(!valid_keys.includes(key)){
+				unknown_keys.push(key)
+			}
+		})
 
-    if(req.body.quote){
-        to_edit.quote = req.body.quote
-    }
+		if(unknown_keys.length > 0){
+			if(unknown_keys.includes("_id")){
+				return res.status(400).json({
+					reason: "Object ID cannot be modified"
+				})
+			}else{
+				return res.status(400).json({
+					reason: `Unknown input field(s) ${unknown_keys}`
+				})
+			}
+		}
 
-    to_edit.save((err) => {
-        if(err){
-            return next(err)
-        }
+		if(req.body.name){
+			if(!typeof(req.body.name) === "string"){
+				return res.status(400).json({
+					reason: "Malformed input data"
+				})
+			}
+			to_edit.name = req.body.name
+		}
 
-        return res.status(200).json(to_edit)
-    })
+		if(req.body.occupation){
+			if(!typeof(req.body.occupation) === "string"){
+				return res.status(400).json({
+					reason: "Malformed input data"
+				})
+			}
+			to_edit.occupation = req.body.occupation
+		}
+
+		if(req.body.quote){
+			if(!typeof(req.body.occupation) === "string"){
+				return res.status(400).json({
+					reason: "Malformed input data"
+				})
+			}
+			to_edit.quote = req.body.quote
+		}
+
+		to_edit.save((err) => {
+			if(err){
+				return next(err)
+			}
+
+			return res.status(200).json(to_edit)
+		})
+	}
 }
 
 export const DeleteCitizenVoice = async (req, res, next) => {
-    const deleted = await CitizenVoiceModel.findByIdAndDelete(req.params.id)
+	const obj_id = req.params.id
 
-    if(deleted){
-        res.status(200).json(deleted)
-    }else{
-        res.sendStatus(404)
-    }
+	if(!mongodb.ObjectID.isValid(obj_id)){
+		return res.status(400).json({
+			reason: "Malformed Object ID"
+		})
+	}else{
+		const deleted = await CitizenVoiceModel.findByIdAndDelete(req.params.id)
+
+		if(deleted){
+			res.status(200).json(deleted)
+		}else{
+			res.sendStatus(404)
+		}
+	}
 }
